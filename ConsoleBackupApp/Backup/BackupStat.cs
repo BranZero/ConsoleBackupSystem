b6d9dcb5077b1bffb223e31ab7fd45faab20c956
BranZero <193959473+BranZero@users.noreply.github.com>
2025-05-21 22:22:08 -0700

    Added Stats to the Backup Controller

--- ConsoleBackupApp/Backup/BackupStat.cs
+++ ConsoleBackupApp/Backup/BackupStat.cs
@@ -2,11 +2,22 @@ namespace ConsoleBackupApp.Backup;
 
 public struct BackupStat
 {
+    private const long MEGABYTE_TO_BYTE = 1_048_576;
     public long Size;
     public int FileCalls;
     public BackupStat(long fileSize = 0, int fileCalls = 0)
     {
         Size = fileSize;
         FileCalls = fileCalls;
+    }
+
+    public static BackupStat operator +(BackupStat a, BackupStat b)
+    {
+        return new BackupStat(a.Size + b.Size, a.FileCalls + b.FileCalls);
+    }
+
+    public readonly long GetSizeInMegaBytes()
+    {
+        return Size / MEGABYTE_TO_BYTE;
     }
 }
