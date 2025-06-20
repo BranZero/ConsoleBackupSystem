588ca3c13289dec4d3831d3267d47c6390c956e7
BranZero <193959473+BranZero@users.noreply.github.com>
2025-06-19 22:08:25 -0700

    Refactor Results

---
+++ ConsoleBackupApp/ResultMessage.cs
@@ -0,0 +1,35 @@
+
+namespace ConsoleBackupApp;
+
+public readonly struct Result(ResultType outcome, string? msg = null)
+{
+    public readonly ResultType ResultType = outcome;
+    public readonly string? Message = msg;
+
+    public readonly string GetMessage()
+    {
+        return $"{ResultType.ToString().Replace('_', ' ')}" + Message != null ? $": {Message}" : "";
+    }
+}
+
+public enum ResultType
+{
+    Success,
+    Info,
+    Warning,
+    Error,
+
+    //Path Results
+    Not_Found,
+    SubPath_Or_SamePath,
+
+    //Argument Issues
+    Too_Few_Arguments,
+    Too_Many_Arguments,
+
+    //Options
+    Invalid_Option,
+    Duplicate_Option,
+    Valid_Option,
+    No_Options,
+}
